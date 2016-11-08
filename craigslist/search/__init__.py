0f9b70d28cec6dc4419f7c6ac0dda0fd75238d59
Al Johri <al.johri@gmail.com>
2016-11-07 22:34:13 -0500

    switch to hhh instead of apa for now

--- craigslist/search/__init__.py
+++ craigslist/search/__init__.py
@@ -7,7 +7,7 @@ def get_url_base(city):
 def get_query_url(city, search_type, offset=0, sort="date", **kwargs):
     params = {"s": offset, "sort": sort, **kwargs}
     params = {k:v for k,v in params.items() if v is not None}
-    url = get_url_base(city) + "/{}/apa?{}".format(search_type, urlencode(params))
+    url = get_url_base(city) + "/{}/hhh?{}".format(search_type, urlencode(params))
     return url
 
 from craigslist.search.jsonsearch import query_jsonsearch
