8d1274b117184305f699ad9abd796ca9c71b5f40
Al Johri <al.johri@gmail.com>
2016-11-11 01:21:10 -0500

    add more tests

--- tests/test_craigslist.py
+++ tests/test_craigslist.py
@@ -1,4 +1,5 @@
 import vcr
+import pytest
 from craigslist import search
 
 @vcr.use_cassette()
@@ -6,7 +7,31 @@ def test_search_apa():
     gen = search('washingtondc', 'apa', postal=20071, search_distance=1)
     post = next(gen)
 
+@vcr.use_cassette(mode='new_episodes')
+def test_search_apa_with_clusters():
+    from itertools import islice
+    gen = search('washingtondc', 'apa', postal=20071, search_distance=1)
+    for post in islice(gen, 0, 110): # force getting at least one cluster
+        pass
+
 @vcr.use_cassette()
 def test_search_sss():
     gen = search('vancouver', 'sss', query='shoes', condition=[10,20], hasPic=1, max_price=20)
     post = next(gen)
+
+def test_get_url_base():
+    from craigslist._search import get_url_base
+    assert get_url_base('washingtondc') == 'https://washingtondc.craigslist.org'
+    assert get_url_base('aberdeen') == 'https://aberdeen.craigslist.co.uk'
+    with pytest.raises(ValueError) as e_info:
+        get_url_base('asdadf')
+
+def test_import_class():
+    from craigslist.utils import import_class
+    from concurrent.futures import ThreadPoolExecutor
+    assert import_class('concurrent.futures.ThreadPoolExecutor') == ThreadPoolExecutor
+    with pytest.raises(ValueError) as e_info:
+        import_class('JustAClassNameWithNoPath')
+    with pytest.raises(ImportError) as e_info:
+        import_class('mypath.that.doesntexist.ImaginaryClass')
+
