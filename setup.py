47fdf650d72bd55f7dfac4830111f064b2f56aec
Al Johri <al.johri@gmail.com>
2016-11-13 17:52:36 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from setuptools import setup
+#!/usr/bin/env python3
+
+import pypandoc
+from setuptools import setup, find_packages
+
+long_description = pypandoc.convert('README.md', 'rst')
+long_description = long_description.replace("\r","")
 
 setup(
     name='craigslist',
     version='0.1.0',
     description='Python wrapper for craigslist.',
+    long_description=long_description,
     author='Al Johri',
     author_email='al.johri@gmail.com',
     url='https://github.com/AlJohri/craigslist',
     license='MIT',
-    packages=['craigslist', 'craigslist.api', 'craigslist._search', 'craigslist._search.jsonsearch', 'craigslist._search.regularsearch'],
+    packages=find_packages(),
     package_data={'craigslist': ['data/*.json']},
     install_requires=['requests', 'cssselect', 'lxml', 'blessings'],
     entry_points={
