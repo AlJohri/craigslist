df8780a006db5f2932d7b5f9ce26a0b0390d50e4
Al Johri <al.johri@gmail.com>
2016-11-14 10:17:30 -0500

    Update simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from craigslist import search
+import craigslist
 
 def main()
-    for post in search('washingtondc', 'apa', postal=20071, search_distance=1):
+    for post in craigslist.search('washingtondc', 'apa', postal=20071, search_distance=1):
         print(post)
 
 if __name__ == '__main__':
